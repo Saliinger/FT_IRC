5047e1b26226b1b02dae31e6dd4caa6b4de967aa
Emeric Krause <ekrause@k1r2p7.42mulhouse.fr>
2025-10-22 17:49:00 +0200

    [channel.cpp/.hpp]

--- include/Channel.hpp
+++ include/Channel.hpp
@@ -1,9 +1,21 @@
 #pragma once
 
+#include <Client.hpp>
+#include <list>
+#include <string>
+
 class Channel
 {
-    public:
-    private:
+	private:
+		std::string					_channelName;
+		std::map<int, Client *>		_clientList;
+
+	public:
+		Channel(std::string channelName);
+		~Channel();
+
+		// Methodes
+		void	addClient(Client& client);
 };
 
 
