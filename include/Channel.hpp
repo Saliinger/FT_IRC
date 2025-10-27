5ebb7e8cddd740a1a7646e163990191cc2fe90b4
Emeric Krause <ekrause@k2r4p5.42mulhouse.fr>
2025-10-27 15:52:29 +0100

    [Channel.hpp/cpp] adding channel operator

--- include/Channel.hpp
+++ include/Channel.hpp
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <map>
+#include <list>
 
 class Client;
 #include <map>
@@ -13,13 +14,15 @@ class Channel
 	private:
 		std::string					_channelName;
 		std::map<int, Client *>		_clientList;
+		std::map<int, Client *>		_operatorList;
 
 	public:
 		Channel(std::string channelName);
 		~Channel();
 
 		// Getters & Setters
-		std::string	getChannelName() const;
+		std::string						getChannelName() const;
+		const std::map<int, Client *>&	getClients() const;
 
 		// Getters & Setters
 		std::string	getChannelName() const;
@@ -27,6 +30,11 @@ class Channel
 		// Methodes
 		void		addClient(Client* client);
 		void		removeClient(Client* client);
+		bool		setOperator(Client* client);
+		bool		removeOperator(Client* client);
+		bool 		isOperator(Client* client) const;
+		// get operators
+		// remove operator
+		// modes
+		// topic
 };
-
-
--- src/Channel.cpp
+++ src/Channel.cpp
@@ -3,9 +3,10 @@
 #include "../include/Client.hpp"
 
 #include <map>
+#include <list>
 #include <string>
 #include <iostream>
-#include <iostream>
+#include <algorithm>
 
 // Constructor & Destructor
 Channel::Channel(std::string channelName)
@@ -20,9 +21,9 @@ Channel::~Channel()
 // Getters
 std::string Channel::getChannelName() const
 { return (_channelName); }
-// Getters
-std::string Channel::getChannelName() const
-{ return (_channelName); }
+
+const std::map<int, Client *>& Channel::getClients() const
+{ return (_clientList); }
 
 // Methodes
 void    Channel::addClient(Client* client)
@@ -30,3 +31,30 @@ void    Channel::addClient(Client* client)
 
 void    Channel::removeClient(Client* client)
 { _clientList.erase(client->getFd()); }
+
+bool    Channel::setOperator(Client* client)
+{
+    if (_clientList.find(client->getFd()) != _clientList.end())
+    {
+        _operatorList[client->getFd()] = client;
+        return (true);
+    }
+    return (false);
+}
+
+bool    Channel::removeOperator(Client* client)
+{
+    if (_operatorList.find(client->getFd()) != _operatorList.end())
+    {
+        _operatorList.erase(client->getFd());
+        return (true);
+    }
+    return (false);
+}
+
+bool    Channel::isOperator(Client* client) const
+{ 
+    if (_operatorList.find(client->getFd()) != _operatorList.end())
+        return (true);
+    return (false);
+} 
